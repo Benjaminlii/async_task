ef902d45256365a7d04ba211edd74f6142ed3f56
Benjamin <benjamin_leee@163.com>
2022-11-19 17:18:28 +0800

    delete biz

--- biz/common/interface.go
+++
@@ -1,7 +0,0 @@
-package common
-
-import "context"
-
-type AsyncTaskHandler interface {
-	HandleMessage(ctx context.Context, taskRequest string) (taskResponse interface{}, err error)
-}
--- biz/service/runner/interface.go
+++
@@ -1,9 +0,0 @@
-package runner
-
-import (
-	"github.com/Benjaminlii/async_task/biz/common"
-)
-
-type RunnerService interface {
-	RegisterHandler(handlerMapping map[common.TaskType]*common.AsyncTaskHandler) error
-}
